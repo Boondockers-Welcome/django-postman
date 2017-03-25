bd33358f35f81ad657a1b1d545676b502eb20cc3
Anna Maste <skulegirlanna@gmail.com>
2017-03-24 21:25:44 -0400

    Fix creating notifications for use with pinax-notifications in post_migrate signal.

--- postman/apps.py
+++ postman/apps.py
@@ -3,6 +3,13 @@ Default application configuration.
 In use as of Django 1.7.
 """
 from django.apps import AppConfig
+from django.conf import settings
+from django.db.models import signals
+try:
+    from importlib import import_module
+except ImportError:
+    from django.utils.importlib import import_module  # Django 1.6 / py2.6
+from .signals import create_notice_types
 
 
 class PostmanConfig(AppConfig):
@@ -11,3 +18,8 @@ class PostmanConfig(AppConfig):
     def ready(self):
         from .models import setup
         setup()
+
+        name = getattr(settings, 'POSTMAN_NOTIFIER_APP', 'notification')
+        if name and name in settings.INSTALLED_APPS:
+            # Technically this should have a sender=notification app, but I can't get it to work!
+            signals.post_migrate.connect(create_notice_types)
---
+++ postman/signals.py
@@ -0,0 +1,16 @@
+from django.utils.translation import ugettext_noop as _
+from django.conf import settings
+try:
+    from importlib import import_module
+except ImportError:
+    from django.utils.importlib import import_module  # Django 1.6 / py2.6
+
+
+def create_notice_types(*args, **kwargs):
+    name = getattr(settings, 'POSTMAN_NOTIFIER_APP', 'notification')
+    if name and name in settings.INSTALLED_APPS:
+        notification = import_module(name + '.models')
+        create = notification.NoticeType.create
+        create("postman_rejection", _("Message Rejected"), _("Your message has been rejected"))
+        create("postman_message", _("Message Received"), _("You have received a message"))
+        create("postman_reply", _("Reply Received"), _("You have received a reply"))
